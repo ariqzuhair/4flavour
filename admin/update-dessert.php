633f442ddd5905465aa8c351d5db48cf5426a086
Ariq Zuhair Rajali <64723384+ariqzuhair@users.noreply.github.com>
2021-10-24 15:45:00 +0800

    commit

--- admin/update-dessert.php
+++ admin/update-dessert.php
@@ -3,13 +3,8 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <body>
- <?php include __DIR__.'/sidebar.php';?>
-  <link rel="stylesheet" type="text/css" href="css/admin.css">
-      echo "<body style='background-color:#F3E4C9'>";
-      <h1>Update Dessert</h1>
-
-      <br><br>
-
+  <?php include __DIR__.'/sidebar.php';?>
+    <link rel="stylesheet" type="text/css" href="css/addForm.css">
       <?php
         //Check whether the id is set or not
         if(isset($_GET['id']))
@@ -56,35 +51,28 @@
         }
        ?>
 
-      <br><br>
-      <form action="" method="POST" enctype="multipart/form-data">
+      <div class="container">
+		    <div class="title">Update Dessert</div>
+		    <form action="" method="POST" enctype="multipart/form-data">
+			    <div class="user-details">
+				    <div class="input-box">
+					    <span class="details">Title</span>
+					    <input type="text" name="title" value="<?php echo $title; ?>">
+			    	</div> 
 
-          <table class="tbl-full" >
-              <tr>
-                  <td>Title: </td>
-                  <td><input type="text" name="title" value="<?php echo $title; ?>">
-              </td>
-              </tr>
+            <div class="input-box">
+              <span class="details">Description</span>
+              <textarea name="description" rows="5" cols="50"><?php echo $description; ?></textarea>
+			    	</div>
 
-              <tr>
-                <td>Description: </td>
-                <td>
-                  <textarea name="description" rows="5" cols="50"><?php echo $description; ?></textarea>
-                </td>
-              </tr>
+            <div class="input-box">
+					    <span class="details">Price</span>
+					    <input type="number" name="price" value="<?php echo $price; ?>">
+			    	</div>
 
-              <tr>
-                <td>Price: </td>
-                <td>
-                  <input type="number" name="price" value="<?php echo $price; ?>">
-                </td>
-              </tr>
-
-
-              <tr>
-                  <td>Current Image: </td>
-                  <td>
-                    <?php
+            <div class="input-box">
+					    <span class="details">Current Image</span>
+					    <?php
                       if($current_image != "")
                       {
                         //Display the images
@@ -100,94 +88,204 @@
                         echo "<div class='error'>Image Not Added.</div>";
                       }
                      ?>
-                  </td>
-              </tr>
+				    </div>
 
-              <tr>
-                  <td>New Image: </td>
-                  <td>
-                    <input type="file" name="image">
-                  </td>
-              </tr>
+            <div class="input-box">
+					    <span class="details">New Image</span>
+					    <input type="file" name="image">
+				    </div>
 
-              <tr>
-                <td>Category: </td>
-                <td>
-                  <select name="category" >
+            <div class="input-box">
+					    <span class="details">Category</span>
+					    <select name="category" >
 
-                    <?php
-                      //create PHP code to display categories from database
-                      //1/ Create SQL to get all active categories from database
-                      $sql = "SELECT * FROM tbl_category WHERE active='Yes'";
+                <?php
+                  //create PHP code to display categories from database
+                  //1/ Create SQL to get all active categories from database
+                  $sql = "SELECT * FROM tbl_category WHERE active='Yes'";
 
-                      //Executing query
-                      $res = mysqli_query($conn, $sql);
+                  //Executing query
+                  $res = mysqli_query($conn, $sql);
 
-                      //Count rows to check whether we have categories or not
-                      $count = mysqli_num_rows($res);
+                  //Count rows to check whether we have categories or not
+                  $count = mysqli_num_rows($res);
 
-                      //If count is greater than zero, we have categories else we do not have categories
-                      if($count>0)
+                  //If count is greater than zero, we have categories else we do not have categories
+                  if($count>0)
+                  {
+                    //We have categories
+                    while($row = mysqli_fetch_assoc($res))
+                    {
+                      //get the details of categories
+                      $category_id = $row['id'];
+                      $category_title = $row['title'];
+
+                      ?>
+
+                      <option <?php if($current_category==$category_id){echo "selected";} ?> value="<?php echo $category_id; ?>"><?php echo $category_title;  ?></option>
+
+                      <?php
+                    }
+                  }
+                  else
                       {
-                        //We have categories
-                        while($row = mysqli_fetch_assoc($res))
-                        {
-                          //get the details of categories
-                          $category_id = $row['id'];
-                          $category_title = $row['title'];
-
-                          ?>
-
-                          <option <?php if($current_category==$category_id){echo "selected";} ?> value="<?php echo $category_id; ?>"><?php echo $category_title;  ?></option>
-
-                          <?php
-                        }
+                        //We do not have categories
+                        ?>
+                        <option value="0">No Dessert Found</option>
+                        <?php
                       }
-                      else
-                      {
-                          //We do not have categories
-                          ?>
-                          <option value="0">No Dessert Found</option>
-                          <?php
-
-                      }
-
                       //2. Display on dropdown
-
                      ?>
-
                   </select>
-                </td>
-              </tr>
+			    	</div>
+		    	</div>
 
-              <tr>
-                  <td>Featured: </td>
-                  <td>
-                    <input <?php if($featured=="Yes"){echo "checked";} ?> type="radio" name="featured" value="Yes"> Yes
-                    <input <?php if($featured=="No"){echo "checked";} ?> type="radio" name="featured" value="No"> No
+            
 
-                  </td>
-              </tr>
+          <div class="featured-details">
+              <input <?php if($featured=="Yes"){echo "checked";} ?> type="radio" name="featured" value="Yes" id="dot-1">
+              <input <?php if($featured=="No"){echo "checked";} ?> type="radio" name="featured" value="no" id="dot-2">
 
-              <tr>
-                  <td>Active: </td>
-                  <td>
-                    <input <?php if($active=="Yes"){echo "checked";} ?> type="radio" name="active" value="Yes"> Yes
-                    <input <?php if($active=="No"){echo "checked";} ?> type="radio" name="active" value="No"> No
+              <span class="featured-title">Featured</span>
+              <div class="category">
+                <label for="dot-1">
+                  <span class="dot one"></span>
+                  <span class="yn">Yes</span>
+                </label>
 
-                  </td>
-              </tr>
+                <label for="dot-2">
+                  <span class="dot two"></span>
+                  <span class="yn">No</span>
+                </label>
+              </div>
+          </div>
 
-              <tr>
-                  <td>
-                      <input type="hidden" name="current_image" value="<?php echo $current_image; ?>">
-                      <input type="hidden" name="id" value="<?php echo $id; ?>">
-                      <input type="submit" name="submit" value="Update Dessert" class="btn-update">
-                  </td>
-              </tr>
-            </table>
-          </form>
+          <div class="featured-details">
+              <input <?php if($active=="Yes"){echo "checked";} ?> type="radio" name="active" value="Yes" id="dot-3">
+              <input <?php if($active=="No"){echo "checked";} ?> type="radio" name="active" value="no" id="dot-4">
+              <span class="featured-title">Active</span>
+              <div class="category">
+                <label for="dot-3">
+                  <span class="dot three"></span>
+                  <span class="yn">Yes</span>
+                </label>
 
+                <label for="dot-4">
+                  <span class="dot four"></span>
+                  <span class="yn">No</span>
+                </label>
+              </div>
+          </div>
+          <div class="button">
+            <input type="hidden" name="current_image" value="<?php echo $current_image; ?>">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <input type="submit" name="submit" value="Update Dessert" class="btn-update">
+			    </div>
+
+          <?php
+            if(isset($_POST['submit']))
+            {
+              //echo "Clicked";
+              //1. Get all the values from our form
+              $id = $_POST['id'];
+              $title = $_POST['title'];
+              $current_image = $_POST['current_image'];
+              $featured = $_POST['featured'];
+              $active = $_POST['active'];
+
+              //2. Updating New Image if selected
+              if(isset($_FILES['image']['name']))
+              {
+                //Get image detail
+                $image_name = $_FILES['image']['name'];
+
+                //Check whether the image is available or not
+                if($image_name != "")
+                {
+                  //Image available
+                  //Upload the new image
+                  $source_path = $_FILES['image']['tmp_name'];
+
+                  $destination_path = "../images/category/".$image_name;
+
+                  //Finally upload the image
+                  $upload = move_uploaded_file($source_path, $destination_path);
+
+                  //Check whether the image is uploaded or not
+                  //And if the image is not uploaded then we will stop the process and redirect with error message
+                  if($upload==false)
+                  {
+                    //Set message
+                    $_SESSION['upload'] = "<div class='error'>Failed to upload image.</div>";
+                    //Redirect to Add Category Page
+                    header('location:'.SITEURL.'admin/manage-category.php');
+                    //Stop the process
+                    die();
+                  }
+
+                  //Remove the current image if available
+                  if($current_image != "")
+                  {
+                    $remove_path = "../images/category/".$current_image;
+
+                    $remove = unlink($remove_path);
+
+                    //Check whether the image is removed or not
+                    //If failed to remove them display message and stop process
+                    if($remove==false)
+                    {
+                      //Failed to remove image
+                      $_SESSION['failed-remove']="<div class='error'>Failed to remove current Image.</div>";
+                      header('location:'.SITEURL.'admin/manage-category.php');
+                      die();
+                  }
+
+                  }
+
+                }
+                else
+                {
+                  $image_name = $current_image;
+                }
+              }
+              else
+              {
+                $image_name = $current_image;
+              }
+
+
+              //3. Update the database
+              $sql2 = "UPDATE tbl_category SET
+                title = '$title',
+                image_name = '$image_name',
+                featured = '$featured',
+                active = '$active'
+                WHERE id=$id
+              ";
+
+              //Execute the query
+              $res2 = mysqli_query($conn, $sql2);
+
+              //4. Redirect to manage category with message
+              //Check whether executed or not
+              if($res2==true)
+              {
+                //Category Updated
+                $_SESSION['update'] = "<div class='success'>Category Updated Successfully.</div>";
+                header('location:'.SITEURL.'admin/manage-category.php');
+              }
+              else
+              {
+                //Failed to update category
+                $_SESSION['update'] = "<div class='error'>Failed to Update Category.</div>";
+                header('location:'.SITEURL.'admin/manage-category.php');
+              }
+            }
+
+           ?>
+
+        </form>
+      </div>
         <?php
 
           if(isset($_POST['submit']))
